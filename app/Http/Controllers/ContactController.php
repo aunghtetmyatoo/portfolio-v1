235d4cfcd196bfb9f8fd0f37e61e9f8d6844c609
ahmo <aunghtetmyatoo888@gmail.com>
2023-12-04 11:39:13 +0630

    email bot

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Mail\ContactMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
@@ -14,6 +15,14 @@ class ContactController extends Controller
             'name' => 'required',
             'email' => ['required', 'email'],
             'message' => 'required'
+        ]);
+
+        Http::post('https://api.telegram.org/bot' . config('telegram.telegram_bot_token') . '/sendMessage', [
+            'chat_id' => config('telegram.telegram_chat_id'),
+            'text' => 'Name - ' . $validated['name'] . PHP_EOL .
+                'Email - ' . $validated['email'] . PHP_EOL .
+                'Message - ' . $validated['message'] . PHP_EOL .
+                'Date - ' . now()->format('Y-m-d H:i:s')
         ]);
 
         // Mail::to('aunghtetmyatoo888@gmail.com')
